3820d01633b23ef515727bc679b92ab821d0ba00
LucyHe <lucy@bit.edu.cn>
2025-12-17 19:06:40 +0800

    添加是否启用传感器的变量

--- main.cpp
+++ main.cpp
@@ -8,15 +8,18 @@ int main() {
     std::string file_dir = config["file_dir"].as<std::string>();
     std::string file_without_suffix = config["file_without_suffix"].as<std::string>();
 
+    int enable_realsense = config["enable_realsense"].as<int>();
+    int enable_prophesee = config["enable_prophesee"].as<int>();
+
     RealSense d455 = RealSense(run_mode, file_dir, file_without_suffix);
     Prophesee ekv4 = Prophesee(run_mode, file_dir, file_without_suffix);
 
     if (run_mode == RunMode::RECORD) {
-        std::jthread t1([&]() { d455.record(); });
-        std::jthread t2([&]() { ekv4.record(); });
+        if (enable_realsense) std::jthread t1([&]() { d455.record(); });
+        if (enable_prophesee) std::jthread t2([&]() { ekv4.record(); });
     } else if (run_mode == RunMode::PLAY) {
-        std::jthread t1([&]() { d455.play(); });
-        std::jthread t2([&]() { ekv4.play(); });
+        if (enable_realsense) std::jthread t1([&]() { d455.play(); });
+        if (enable_prophesee) std::jthread t2([&]() { ekv4.play(); });
     } else {
         printf("错误! Run Mode 未定义\n");
         return -1;
